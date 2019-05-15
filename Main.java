448e8e5725dfb71d63b753ba8e39928bcca85f92
Jaskier234 <jaskier234@gmail.com>
2019-05-15 15:57:30 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,32 +7,32 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket socket = new ServerSocket(7890);
+        Socket client = socket.accept();
+        DataInputStream inputStream = new DataInputStream(client.getInputStream());;
+        DataOutputStream outputStream = new DataOutputStream(client.getOutputStream());
 //
 //        try
 //        {
-//            ServerSocket socket = new ServerSocket(7890);
+//            socket = new ServerSocket(7890);
 //
+//            System.out.println("Czekam na połączenie");
 //            Socket client = socket.accept();
-//
+//            inputStream =
+//            outputStream = ;
+//            System.out.println("Połączono!");
 //        }
 //        catch(Exception e)
 //        {
 //            System.out.println("socket error");
 //        }
-//        
+
         JFrame f=new JFrame();//creating instance of JFrame
 
         JButton b=new JButton("click");//creating instance of JButton
         b.setBounds(0,400,400, 100);//x axis, y axis, width, height
-
-        b.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Clicked");
-            }
-        });
 
         JTextField text = new JTextField("teskt");
         text.setVisible(true);
@@ -50,5 +50,28 @@ public class Main {
         f.setSize(500,600);//400 width and 500 height
         f.setLayout(null);//using no layout managers
         f.setVisible(true);//making the frame visible
+
+        b.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = input.getText();
+                System.out.println(message);
+                try {
+                    outputStream.writeChars(message);
+                    outputStream.flush();
+                    System.out.println("wysłano: "+message);
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                text.setText(text.getText()+ "\n" + message);
+                input.setText("");
+            }
+        });
+
+        while(true)
+        {
+//            System.out.println("petla");
+        }
     }
 }
